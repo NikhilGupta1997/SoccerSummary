679f78dadfe48ce4aa43a60d380aa249c1459347
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2018-04-13 14:55:18 +0530

    Take match id information

--- summary.py
+++ summary.py
@@ -3,3 +3,9 @@
 # 	Copyright © Nikhil Gupta & Ayush Bhardwaj	#
 #################################################
 
+import numpy as np 
+import sys
+
+# Get match number to summarize
+match_id = int(sys.argv[1])
+print match_id
