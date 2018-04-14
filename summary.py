be498b74ed1377105a9067d961cb64d6b5ab0113
Ayush Bhardwaj <ayushbhardwaj@Ayushs-MacBook-Pro.local>
2018-04-14 15:18:20 +0530

    Function to find dominance periods added

--- summary.py
+++ summary.py
@@ -39,7 +39,6 @@ with open("./football-events/ginf.csv") as f_obj:
 # (Home team, Away team, Home Team Score, Away Team Score, odds_home, odds_draw, odds_against)
 def get_match_info(match_id):
 	info = meta_data[match_id]
-	# return (info['ht'], info['at'], info['fthg'], info['ftag'], info['odd_h'], info['odd_d'], info['odd_a'])
 	return info
 # returns (winner:(0:home/1:draw/2:away), expectation(0:expected, 1:close, 2:shock, 3:unbelievable) 
 # Add features like close match, high scoring
@@ -88,12 +87,55 @@ def start_line(match_info, match_commentary):
 
 	return start_str
 
+# Return list of tuples (dominating_tema(0: home, 1 : away), after_goal or not(0-> no goal, 1-> home goal, 2->away goal), start time, end time) 
+def find_dominance(match_info, match_commentary):
+	attacking_opportunities = [1, 2, 8]
+	ans = []
+	print type(match_commentary), len(match_commentary)
+	period_gap = 5
+	period_start = 0
+	dominance_diff = 2
+
+	home_team = match_info['ht']
+	away_team = match_info['at']
+	
+	home_attacks_count = 0
+	away_attacks_count = 0
+
+	for event in match_commentary:
+		if(int(event['time']) > (period_start + period_gap)):
+			prev_dominance = home_attacks_count - away_attacks_count
+			if(prev_dominance > dominance_diff):
+				ans.append((0, 0, period_start, period_start + period_gap))
+			if(prev_dominance < -1 * dominance_diff):
+				ans.append((1, 0, period_start, period_start + period_gap))	
+
+			period_start += period_gap
+			home_attacks_count = 0
+			away_attacks_count = 0
+
+		if(int(event['event_type']) in attacking_opportunities):
+			if(event['event_team'] == home_team):
+				home_attacks_count += 1
+			else:
+				away_attacks_count += 1	
+	return ans
+	
+	# After and Before a Goal domination also
+	
+
+	
+				
+
+
+
 
 match_info = get_match_info('UFot0hit/')
-match_commentary = meta_data['UFot0hit/']
+match_commentary = text_data['UFot0hit/']
 
 print match_info
 print winner(match_info)
 print start_line(match_info, match_commentary)
+print find_dominance(match_info, match_commentary)
 
 
